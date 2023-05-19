99eead4d954d530775a1bb3693eddaf508b4eb3f
Marie Metay <mariemetay.pro@gmail.com>
2023-05-19 14:45:43 +0200

    Add facebook, mail and twitter connection

--- app/src/main/java/com/example/projet7/HomeActivity.java
+++ app/src/main/java/com/example/projet7/HomeActivity.java
@@ -4,18 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.projet7.databinding.ActivityHomeBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.firebase.auth.FirebaseAuth;
+import com.firebase.ui.auth.AuthUI;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
-
-    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +20,15 @@ public class HomeActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        mGoogleSignInClient = GoogleSignIn.getClient(HomeActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
-
         binding.btnLogOut.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            finish();
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            finish();
+                        } else {
+                            Toast.makeText(HomeActivity.this, "error logout", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
     }
 }
--- app/src/main/java/com/example/projet7/MainActivity.java
+++ app/src/main/java/com/example/projet7/MainActivity.java
@@ -1,72 +1,48 @@
 package com.example.projet7;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.projet7.databinding.ActivityMainBinding;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    // Google
-    GoogleSignInClient mGoogleSignInClient;
     FirebaseAuth mFirebaseAuth;
-    ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
-                        if (signInAccountTask.isSuccessful()) {
-                            try {
-                                GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
-                                if (googleSignInAccount != null) {
-                                    AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
-                                    mFirebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<AuthResult> task) {
-                                            if (task.isSuccessful()) {
-                                                startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                                            } else {
-                                                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                                                Log.d("INFO", task.getException().getMessage());
-                                            }
-                                        }
-                                    });
-                                }
-                            } catch (ApiException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
     );
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.FacebookBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build(),
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.TwitterBuilder().build()
+    );
+
+    Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.logo)
+            .setTheme(R.style.LoginTheme)
+            .setIsSmartLockEnabled(false)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,25 +52,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+    }
 
-        // Google
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, googleSignInOptions);
-
-        binding.btnGoogle.setOnClickListener(v -> {
-            Intent intent = mGoogleSignInClient.getSignInIntent();
-            resultLauncher.launch(intent);
-        });
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Error to connect", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-        if (firebaseUser != null) {
+        if (firebaseUser == null) {
+            signInLauncher.launch(signInIntent);
+        } else {
             startActivity(new Intent(MainActivity.this, HomeActivity.class));
         }
     }
