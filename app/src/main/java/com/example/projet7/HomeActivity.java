6bde4ff6581ea0f42cf3fafad5be045447cbc14f
Marie Metay <mariemetay.pro@gmail.com>
2023-06-12 15:52:46 +0200

    Modify navigation

--- app/src/main/java/com/example/projet7/HomeActivity.java
+++ app/src/main/java/com/example/projet7/HomeActivity.java
@@ -3,30 +3,18 @@ package com.example.projet7;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
-import android.content.pm.PermissionInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.example.projet7.data.OkhttpService;
 import com.example.projet7.databinding.ActivityHomeBinding;
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -39,8 +27,9 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        setSupportActionBar(binding.toolbar);
         permissionLauncher.launch(permission);
+
+
 
 //        binding.btnLogOut.setOnClickListener(v -> {
 //            AuthUI.getInstance().signOut(this)
@@ -52,27 +41,6 @@ public class HomeActivity extends AppCompatActivity {
 //                        }
 //                    });
 //        });
-
-        binding.navigationBar.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.map_view) {
-                replaceFragment(new MapViewFragment());
-                binding.toolbar.setTitle(R.string.toolbar_title_view);
-            } else if (item.getItemId() == R.id.list_view) {
-                replaceFragment(new ListViewFragment());
-                binding.toolbar.setTitle(R.string.toolbar_title_view);
-            } else if (item.getItemId() == R.id.workmates) {
-                replaceFragment(new WorkmatesFragment());
-                binding.toolbar.setTitle(R.string.toolbar_title_workmates);
-            }
-            return true;
-        });
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, fragment);
-        fragmentTransaction.commit();
     }
 
     private ActivityResultLauncher<String> permissionLauncher = registerForActivityResult(
@@ -81,7 +49,12 @@ public class HomeActivity extends AppCompatActivity {
                 @Override
                 public void onActivityResult(Boolean isGranted) {
                     if (isGranted) {
-                        replaceFragment(new MapViewFragment());
+                        setSupportActionBar(binding.toolbar);
+                        NavController navController = Navigation.findNavController(HomeActivity.this, R.id.nav_host_fragment);
+                        NavigationUI.setupWithNavController(binding.navigationBar, navController);
+                        NavigationUI.setupWithNavController(binding.toolbar, navController);
+                        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_map, R.id.nav_list, R.id.nav_workmates).build();
+                        NavigationUI.setupActionBarWithNavController(HomeActivity.this, navController, appBarConfiguration);
                     } else {
                         Log.d("Permission", "permission denied");
                     }
